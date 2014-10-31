60cc55487e97ec94ca9f3d56dcd0951ee5d60b9d
Akash Nimare <svnitakash@gmail.com>
2014-11-01 01:33:20 +0530

    Updated docs

--- hide.js
+++ hide.js
@@ -1,4 +1,6 @@
 $(function () {
  	 
     $(".content_item:contains('Indian'), .content_item:contains('India')").hide();
+    //It will hide everything which contains Indian, India.
+    //You can also add name of any Quora user.
 });
